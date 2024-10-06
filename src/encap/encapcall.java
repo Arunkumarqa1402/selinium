85ee0994a69eacac77d5ac50c7acc4fe618aed90
syss <syss@sys.lan>
2024-10-06 15:29:00 +0530

    my project

---
+++ src/encap/encapcall.java
@@ -0,0 +1,31 @@
+package encap;
+
+public class encapcall {
+
+	
+	
+	
+	static{
+		
+		System.out.println("i am static block");
+		
+	}
+	
+	
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		/*encapsulation obj =new encapsulation();
+		
+		obj.setAccountno(1000);
+		
+		System.out.println(obj.getAccountno());*/
+		
+		
+
+	}
+
+}
---
+++ src/encap/encapsulation.java
@@ -0,0 +1,48 @@
+package encap;
+
+public class encapsulation {
+	
+	
+	
+	private int accountno;
+	
+	private String name;
+
+	private int karthik;
+	 private  int pradeep;
+	public int getAccountno() {
+		return accountno;
+	}
+	public void setAccountno(int accountno) {
+		this.accountno = accountno;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getKarthik() {
+		return karthik;
+	}
+	public void setKarthik(int karthik) {
+		this.karthik = karthik;
+	}
+	public int getPradeep() {
+		return pradeep;
+	}
+	public void setPradeep(int pradeep) {
+		this.pradeep = pradeep;
+	}
+	
+		
+	
+	
+	
+	
+	
+	
+
+		}
+
+
